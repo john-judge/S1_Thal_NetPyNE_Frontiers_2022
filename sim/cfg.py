35f31a4333f3426a8c326b9c08686bded1c7c1bc
john-judge <jjudge@uchicago.edu>
2025-02-28 15:39:12 -0600

    recordTraces: only soma now, see if still segfaults

--- sim/cfg.py
+++ sim/cfg.py
@@ -159,8 +159,8 @@ cfg.recordTraces['Vsoma'] = {'sec':'soma','loc':0.5,'var':'v'}
     #cfg.recordTraces['Vsoma_'+str(i)] = {'sec':'soma_'+str(i),'loc':0.5,'var':'v'}
     #cfg.recordTraces['Vdend_'+str(i)] = {'sec':'dend_'+str(i),'loc':0.5,'var':'v'}
     #cfg.recordTraces['Vapic_'+str(i)] = {'sec':'apic_'+str(i),'loc':0.5,'var':'v'}
-    cfg.recordTraces['Vaxon_'+str(i)] = {'sec':'axon_'+str(i),'loc':0.5,'var':'v'}
-    
+    #cfg.recordTraces['Vaxon_'+str(i)] = {'sec':'axon_'+str(i),'loc':0.5,'var':'v'}
+
 cfg.recordStim = True			
 cfg.recordTime = True  		
 cfg.recordStep = 0.1            
