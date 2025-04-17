da4dd96a1864ad8afb3c44468a15c9e20601c682
John Judge <jjudge3@wisc.edu>
2025-04-17 18:18:15 -0500

    fix to memmap issue

--- src/hVOS/compartment_file.py
+++ src/hVOS/compartment_file.py
@@ -16,14 +16,15 @@ class MemoryMappedCompartmentVoltages:
 
     def load_existing_mmap(self, hash_map_filename, mmap_filename, shape=(10000,)):
         self.hash_map = pickle.load(open(hash_map_filename, 'rb'))
-        self.mmap_fp = np.require(np.memmap(mmap_filename, dtype='float32', mode='r'), requirements=['O'])
+        self.mmap_fp = np.memmap(mmap_filename, dtype='float32', mode='r')
         self.mmap_fp = self.mmap_fp.reshape(shape)
         self.shape = self.mmap_fp.shape
         self.mmap_filename = mmap_filename
 
     def init_mmap(self, arr_shape):
+        print("Initializing memory mapped file with shape", (self.fp_size_init,) + arr_shape)
         self.shape = (self.fp_size_init,) + arr_shape
-        self.mmap_fp = np.memmap(self.mmap_filename, dtype='float32', mode='w+', shape=self.shape)
+        self.mmap_fp = np.require(np.memmap(self.mmap_filename, dtype='float32', mode='w+', shape=self.shape), requirements=['O'])
 
     def dump_hash_map(self, hash_map_filename):
         with open(hash_map_filename, 'wb') as f:
@@ -36,6 +37,7 @@ class MemoryMappedCompartmentVoltages:
         np.save(self.mmap_filename, self.mmap_fp, allow_pickle=False)
 
     def resize_mmap(self, arr_shape):
+        print("Resizing memory mapped file to shape", (self.shape[0] * 2,) + arr_shape)
         self.shape = (self.shape[0] * 2,) + arr_shape
         self.mmap_fp.resize(self.shape)
 
