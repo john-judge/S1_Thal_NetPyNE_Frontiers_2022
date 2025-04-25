c2cf548bda5a3ae714beadfd40a9ba33d6d619d7
John Judge <jjudge3@uchicago.edu>
2025-04-25 15:11:01 -0500

    run8 tune3: scale to 0.25, soma:dend 1.0, density 1.0

--- sim/cfg.py
+++ sim/cfg.py
@@ -250,11 +250,11 @@ cfg.analysis['plotShape'] = {'includePre': [8008, 8239],
 #------------------------------------------------------------------------------
 
  # Number of cells = 31346 
-cfg.scale = 0.1 # reduce size
+cfg.scale = 0.25 # reduce size
 cfg.sizeY = 2082.0
 cfg.sizeX = 420.0 # r = 210 um and hexagonal side length = 230.9 um
 cfg.sizeZ = 420.0
-cfg.scaleDensity = 2.0 # run 8.1: increase density of cells by 2x
+cfg.scaleDensity = 1.0 # run 8.1: increase density of cells by 2x
 
 
 #------------------------------------------------------------------------------
