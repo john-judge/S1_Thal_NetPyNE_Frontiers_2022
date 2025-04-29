5c676141a40e4d59ec0a1ed32274a6062afe3cc5
john-judge <jjudge@uchicago.edu>
2025-04-29 12:32:35 -0500

    zoom in to cam res 1.0 for run8_tune9

--- cam_params.py
+++ cam_params.py
@@ -3,7 +3,7 @@
 cam_params = {
     'cam_width': 200,
     'cam_height': 200,
-    'cam_resolution': 3.0,
+    'cam_resolution': 1.0,
     'cam_fov': [185.,800.,64.],  # or give index of cell to center on
     'xstim_at_cam_fov': True
 }
