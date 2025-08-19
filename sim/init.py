63042596b099a789a5803f2d1c83d0bc2ac78d1a
john-judge <jjudge@uchicago.edu>
2025-08-19 10:11:23 -0500

    fix attach x-stim to use vext to apply stimulation

--- sim/init.py
+++ sim/init.py
@@ -47,7 +47,7 @@ attach_xstim_to_segments_mpi_safe(
 )
 
 
-#sim.net.addStims() 							# add network stimulation
+sim.net.addStims() 							# add network stimulation
 sim.setupRecording()              			# setup variables to record for each cell (spikes, V traces, etc)
 sim.runSim()                      			# run parallel Neuron simulation  
 sim.gatherData()                  			# gather spiking data and cell info from each node
