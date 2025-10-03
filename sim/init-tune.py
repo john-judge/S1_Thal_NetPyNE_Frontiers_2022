16c535802303b9016226cea6bc3af8bf8026889a
john-judge <jjudge@uchicago.edu>
2025-10-03 14:45:52 -0500

    disable parallelism for tuning

--- sim/init-tune.py
+++ sim/init-tune.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 import os
 """
 init.py
@@ -62,7 +61,7 @@ else:
 # ACSF trial first (no blockade; experiment_NBQX_global should be set to False in cfg-tune.py)
 sim.runSim()                      			# run parallel Neuron simulation  
 sim.gatherData()                  			# gather spiking data and cell info from each node
-acsf_data = deepcopy(sim.allSimData)  # save ACSF data
+acsf_data = dict(sim.allSimData) # save ACSF data
 sim.allSimData = {}
 print("Finished ACSF trial and copied data")
 sim.setupRecording()
@@ -73,7 +72,7 @@ set_syn_blockade(fraction=fraction_blockade)
 print(f"Set synaptic blockade to {fraction_blockade} (0=full NBQX, 1=ACSF)")
 sim.runSim()                     
 sim.gatherData()  
-nbqx_data = deepcopy(sim.allSimData)  # save NBQX data
+nbqx_data = dict(sim.allSimData)  # save NBQX data
 sim.allSimData = {'nbqx': nbqx_data, 'acsf': acsf_data}
 print('Finished both ACSF and NBQX trials')
 
