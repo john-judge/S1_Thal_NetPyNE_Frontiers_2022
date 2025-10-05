8245d62c49089b785b38e7bea656e0bb20456594
john-judge <jjudge@uchicago.edu>
2025-10-04 19:12:00 -0500

    try without file save net

--- sim/init-tune.py
+++ sim/init-tune.py
@@ -92,7 +92,7 @@ nbqx_data['t'] = sim.allSimData['t']'''
 sim.allSimData = {'simData': {'acsf': acsf_data, 'nbqx': nbqx_data}}
 sim.saveData()
 
-if not os.path.exists(netFile):
+'''if not os.path.exists(netFile):
     # Save network for future runs
     os.makedirs(cfg.simLabel, exist_ok=True)
     cfg.filename = 'base_net_tuning'
@@ -100,6 +100,6 @@ if not os.path.exists(netFile):
     sim.saveData(filename=netFile, include=['net', 'simConfig', 'netParams'])  # save net and cfg only
     print(f"[Node 0] Network saved to 'base_net_tuning.pkl' in {cfg.simLabel}")
 
-
+'''
 print('Finished both ACSF and NBQX trials')
 
