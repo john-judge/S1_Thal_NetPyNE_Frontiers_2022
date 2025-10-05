6ec75aac673d957b854c423f59113b8d974acfbb
john-judge <jjudge@uchicago.edu>
2025-10-04 22:42:23 -0500

    no reusing nets; build net each time for each sim

--- sim/init-tune.py
+++ sim/init-tune.py
@@ -7,6 +7,7 @@ Starting script to tune ACSF and NBQX simulations of NetPyNE-based S1 model.
 
 import matplotlib; matplotlib.use('Agg')  # to avoid graphics error in servers
 from netpyne import sim, specs
+import time
 
 
 def set_syn_blockade(fraction):
@@ -27,17 +28,11 @@ def set_syn_blockade(fraction):
     sim.net.modifyConns(modification_params)
 
 
-
-cfg, netParams = sim.readCmdLineArgs()
-netFile = '../base_net_tuning.pkl'
-if os.path.exists(netFile):
-    print(f"Loading pre-built network from base_net_tuning...")
-    cfg.loadFromFile = True  # ensures network is loaded instead of rebuilt
-    sim.pc.gid_clear() # Clear the network from memory
-    sim.loadSim(netFile)
-else:
-    print(("building network from scratch with netParams"))
-
+def build_network(acsf=True):
+    
+    cfg, netParams = sim.readCmdLineArgs()
+    if not acsf:
+        cfg.experiment_NBQX_global = True
     sim.initialize(
         simConfig = cfg, 	
         netParams = netParams)  				# create network object and set cfg and net params
@@ -50,22 +45,19 @@ else:
     sim.net.addStims() 							# add network stimulation
     sim.setupRecording()              			# setup variables to record for each cell (spikes, V traces, etc)
 
-    sim.saveData(filename=netFile, include=['net'])
 
-    
+# start timer 
+start_time = time.time()
+
+build_network()
 # ACSF trial first (no blockade; experiment_NBQX_global should be set to False in cfg-tune.py)
 sim.cfg.filename = 'acsf_run'
 sim.runSim()                      			# run parallel Neuron simulation  
 sim.gatherData()                  			# gather spiking data and cell info from each node
 acsf_data = dict(sim.allSimData) #dict(sim.allSimData) # save ACSF data, deep copy
 
-# now run NBQX trial
-print(f"Set synaptic blockade to {fraction_blockade} (0=full NBQX, 1=ACSF)")
+build_network(acsf=False)
 sim.cfg.filename = 'nbqx_run'
-# load network from file to reset to ACSF state
-sim.loadNet(netFile)
-set_syn_blockade(fraction=fraction_blockade)
-print("Check that cfg.recordTraces is still set:", cfg.recordTraces)
 sim.runSim()                     
 sim.gatherData()  
 nbqx_data = dict(sim.allSimData)
@@ -73,14 +65,6 @@ nbqx_data = dict(sim.allSimData)
 sim.allSimData = {'simData': {'acsf': acsf_data, 'nbqx': nbqx_data}}
 sim.saveData()
 
-'''if not os.path.exists(netFile):
-    # Save network for future runs
-    os.makedirs(cfg.simLabel, exist_ok=True)
-    cfg.filename = 'base_net_tuning'
-    cfg.savePickle = True 
-    sim.saveData(filename=netFile, include=['net', 'simConfig', 'netParams'])  # save net and cfg only
-    print(f"[Node 0] Network saved to 'base_net_tuning.pkl' in {cfg.simLabel}")
-
-'''
-print('Finished both ACSF and NBQX trials')
+end_time = time.time()
+print(f"Total iteration simulation time (both ACSF and NBQX): {(end_time - start_time)/60} minutes")
 
