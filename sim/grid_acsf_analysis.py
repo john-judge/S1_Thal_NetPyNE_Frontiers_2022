e2a47188c71cbd40a6253a88975147761fa3c3f4
john-judge <jjudge@uchicago.edu>
2025-11-04 10:49:57 -0600

    reduce grid batch size, test again

--- sim/grid_acsf_analysis.py
+++ sim/grid_acsf_analysis.py
@@ -8,9 +8,13 @@ import subprocess
 # To be run on Condor after grid_acsf.py has completed
 # grid_acsf_map[propVelocity] = simData_acsf
 
-data_dir = f'S1_Thal_NetPyNE_Frontiers_2022/grid/'
+data_dir = f'S1_Thal_NetPyNE_Frontiers_2022/grid/grid_acsf/'
 grid_acsf_map = {}
-
+for file in os.listdir(data_dir):
+    if file.endswith('.pkl'):
+        with open(os.path.join(data_dir, file), 'rb') as f:
+            simData_acsf = pickle.load(f)
+            grid_acsf_map[simData_acsf['simConfig']['propVelocity']] = simData_acsf
 
 with open('../../grid_acsf_map.pkl', 'wb') as f:
     pickle.dump(grid_acsf_map, f)
