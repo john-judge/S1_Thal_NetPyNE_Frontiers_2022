cd2e8d39d1d5793258a3996fbe627e9d2e6544c9
john-judge <jjudge@uchicago.edu>
2026-06-08 12:04:16 -0500

    me_type debug

--- analyze_hVOS_parallel.py
+++ analyze_hVOS_parallel.py
@@ -191,10 +191,11 @@ for cell_id in loaded_compart_data.hash_map.keys():
         print("Loading morphology for me_type:", me_type)
         try:
             me_type_part1, me_type_part2, me_type_part3 = me_type.split("_")
-        except ValueError:
-            print("Unexpected me_type format (expected 3 parts separated by underscores):", me_type)
-            continue
-        m_type, e_type = me_type_part1 + "_" + me_type_part2, me_type_part3
+            m_type, e_type = me_type_part1 + "_" + me_type_part2, me_type_part3
+        except ValueError: # example: L6_TPC_L4_cAD
+            me_type_part1, me_type_part2, me_type_part3, me_type_part4 = me_type.split("_")
+            m_type, e_type = me_type_part1 + "_" + me_type_part2+ "_" + me_type_part3, me_type_part4
+        
         me_type_files = [f for f in os.listdir(morphology_data_dir) if (m_type in f and e_type in f and f.endswith('.cell.nml'))]
         if len(me_type_files) == 0:
             assert me_type not in target_hVOS_populations  # we only care if we cannot load a target population
