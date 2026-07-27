724dd666ceea0184926096989b60cca0e0d746d4
john-judge <jjudge@uchicago.edu>
2026-07-27 14:22:50 -0500

    add ACSF run

--- sim/dag_run_map.py
+++ sim/dag_run_map.py
@@ -105,9 +105,9 @@ partial_nbqx_fractions_map = {
 # run 16: re-test some of run15 with 
 # new random seeds to see if the results are consistent
 # or chaotic
-    
+    'run_16_00'
     'run16_00': 0.5,   #
-    'run16_01': 0.51,   #
+    'run16_01': 0.0,   #
     'run16_02': 0.49,   #
     'run16_03': 0.52,   #
     'run16_04': 0.48,   #
